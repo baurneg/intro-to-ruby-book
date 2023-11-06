ef20bd7068f02dc623ce79ba0805ea582a0a9b60
Baur Negmetzhanov <baurneg@gmail.com>
2023-11-06 00:32:29 +0000

    updated exercises

--- 8-more-stuff/1.rb
+++ 8-more-stuff/1.rb
@@ -1,5 +1,3 @@
 #Write a program that checks if the sequence of characters "lab" exists in the following strings. If it does exist, print out the word.
 words = ["laboratory", "experiment", "Pans Labyrinth", "elaborate", "polar bear"]
-  if words.each.include?("lab")
-    puts words[]
-  end
+puts words.select! { |element| element.to_s.include?("lab") }
